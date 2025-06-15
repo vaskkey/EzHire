6434e76042d3707056537668c50384f2b04a203a
Awraam <awraam.waskowski@gmail.com>
2025-06-15 15:55:39 +0200

    🚀 feat: Add emails to candidates

---
+++ ezhire_api/ezhire_api/Migrations/20250615135448_AddEmailToCandidate.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ezhire_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEmailToCandidate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "email",
+                table: "candidates",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "email",
+                table: "candidates");
+        }
+    }
+}
