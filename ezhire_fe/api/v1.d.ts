7d8a2ba921aaacf24f622fcfbf726ea033ec2b1d
Awraam <awraam.waskowski@gmail.com>
2025-06-15 13:29:40 +0200

    ⚙️ chore: Update schema

--- ezhire_fe/api/v1.d.ts
+++ ezhire_fe/api/v1.d.ts
@@ -4,215 +4,343 @@
  */
 
 export interface paths {
-	"/api/Candidates": {
-		parameters: {
-			query?: never;
-			header?: never;
-			path?: never;
-			cookie?: never;
-		};
-		get: {
-			parameters: {
-				query?: never;
-				header?: never;
-				path?: never;
-				cookie?: never;
-			};
-			requestBody?: never;
-			responses: {
-				/** @description OK */
-				200: {
-					headers: {
-						[name: string]: unknown;
-					};
-					content: {
-						"text/plain": components["schemas"]["CandidateGetDto"];
-						"application/json": components["schemas"]["CandidateGetDto"];
-						"text/json": components["schemas"]["CandidateGetDto"];
-					};
-				};
-			};
-		};
-		put?: never;
-		post?: never;
-		delete?: never;
-		options?: never;
-		head?: never;
-		patch?: never;
-		trace?: never;
-	};
-	"/api/campaigns": {
-		parameters: {
-			query?: never;
-			header?: never;
-			path?: never;
-			cookie?: never;
-		};
-		get: {
-			parameters: {
-				query?: never;
-				header?: never;
-				path?: never;
-				cookie?: never;
-			};
-			requestBody?: never;
-			responses: {
-				/** @description OK */
-				200: {
-					headers: {
-						[name: string]: unknown;
-					};
-					content: {
-						"text/plain": components["schemas"]["RecruitmentCampaignGetDto"][];
-						"application/json": components["schemas"]["RecruitmentCampaignGetDto"][];
-						"text/json": components["schemas"]["RecruitmentCampaignGetDto"][];
-					};
-				};
-			};
-		};
-		put?: never;
-		post: {
-			parameters: {
-				query?: never;
-				header?: never;
-				path?: never;
-				cookie?: never;
-			};
-			requestBody: {
-				content: {
-					"application/json": components["schemas"]["RecruitmentCampaignCreateDto"];
-					"text/json": components["schemas"]["RecruitmentCampaignCreateDto"];
-					"application/*+json": components["schemas"]["RecruitmentCampaignCreateDto"];
-				};
-			};
-			responses: {
-				/** @description Created */
-				201: {
-					headers: {
-						[name: string]: unknown;
-					};
-					content: {
-						"text/plain": components["schemas"]["RecruitmentCampaignGetDto"];
-						"application/json": components["schemas"]["RecruitmentCampaignGetDto"];
-						"text/json": components["schemas"]["RecruitmentCampaignGetDto"];
-					};
-				};
-			};
-		};
-		delete?: never;
-		options?: never;
-		head?: never;
-		patch?: never;
-		trace?: never;
-	};
-	"/api/campaigns/{id}": {
-		parameters: {
-			query?: never;
-			header?: never;
-			path?: never;
-			cookie?: never;
-		};
-		get: {
-			parameters: {
-				query?: never;
-				header?: never;
-				path: {
-					id: number;
-				};
-				cookie?: never;
-			};
-			requestBody?: never;
-			responses: {
-				/** @description OK */
-				200: {
-					headers: {
-						[name: string]: unknown;
-					};
-					content: {
-						"text/plain": components["schemas"]["RecruitmentCampaignGetDto"];
-						"application/json": components["schemas"]["RecruitmentCampaignGetDto"];
-						"text/json": components["schemas"]["RecruitmentCampaignGetDto"];
-					};
-				};
-				/** @description Not Found */
-				404: {
-					headers: {
-						[name: string]: unknown;
-					};
-					content: {
-						"text/plain": components["schemas"]["ProblemDetails"];
-						"application/json": components["schemas"]["ProblemDetails"];
-						"text/json": components["schemas"]["ProblemDetails"];
-					};
-				};
-			};
-		};
-		put?: never;
-		post?: never;
-		delete?: never;
-		options?: never;
-		head?: never;
-		patch?: never;
-		trace?: never;
-	};
+    "/api/Candidates": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        get: {
+            parameters: {
+                query?: never;
+                header?: never;
+                path?: never;
+                cookie?: never;
+            };
+            requestBody?: never;
+            responses: {
+                /** @description OK */
+                200: {
+                    headers: {
+                        [name: string]: unknown;
+                    };
+                    content: {
+                        "text/plain": components["schemas"]["CandidateGetDto"];
+                        "application/json": components["schemas"]["CandidateGetDto"];
+                        "text/json": components["schemas"]["CandidateGetDto"];
+                    };
+                };
+            };
+        };
+        put?: never;
+        post?: never;
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
+    };
+    "/api/campaigns": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        get: {
+            parameters: {
+                query?: never;
+                header?: never;
+                path?: never;
+                cookie?: never;
+            };
+            requestBody?: never;
+            responses: {
+                /** @description OK */
+                200: {
+                    headers: {
+                        [name: string]: unknown;
+                    };
+                    content: {
+                        "text/plain": components["schemas"]["RecruitmentCampaignGetDto"][];
+                        "application/json": components["schemas"]["RecruitmentCampaignGetDto"][];
+                        "text/json": components["schemas"]["RecruitmentCampaignGetDto"][];
+                    };
+                };
+            };
+        };
+        put?: never;
+        post: {
+            parameters: {
+                query?: never;
+                header?: never;
+                path?: never;
+                cookie?: never;
+            };
+            requestBody: {
+                content: {
+                    "application/json": components["schemas"]["RecruitmentCampaignCreateDto"];
+                    "text/json": components["schemas"]["RecruitmentCampaignCreateDto"];
+                    "application/*+json": components["schemas"]["RecruitmentCampaignCreateDto"];
+                };
+            };
+            responses: {
+                /** @description Created */
+                201: {
+                    headers: {
+                        [name: string]: unknown;
+                    };
+                    content: {
+                        "text/plain": components["schemas"]["RecruitmentCampaignGetDto"];
+                        "application/json": components["schemas"]["RecruitmentCampaignGetDto"];
+                        "text/json": components["schemas"]["RecruitmentCampaignGetDto"];
+                    };
+                };
+            };
+        };
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
+    };
+    "/api/campaigns/{id}": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        get: {
+            parameters: {
+                query?: never;
+                header?: never;
+                path: {
+                    id: number;
+                };
+                cookie?: never;
+            };
+            requestBody?: never;
+            responses: {
+                /** @description OK */
+                200: {
+                    headers: {
+                        [name: string]: unknown;
+                    };
+                    content: {
+                        "text/plain": components["schemas"]["RecruitmentCampaignGetDto"];
+                        "application/json": components["schemas"]["RecruitmentCampaignGetDto"];
+                        "text/json": components["schemas"]["RecruitmentCampaignGetDto"];
+                    };
+                };
+                /** @description Bad Request */
+                400: {
+                    headers: {
+                        [name: string]: unknown;
+                    };
+                    content: {
+                        "text/plain": components["schemas"]["ProblemDetails"];
+                        "application/json": components["schemas"]["ProblemDetails"];
+                        "text/json": components["schemas"]["ProblemDetails"];
+                    };
+                };
+                /** @description Not Found */
+                404: {
+                    headers: {
+                        [name: string]: unknown;
+                    };
+                    content: {
+                        "text/plain": components["schemas"]["ProblemDetails"];
+                        "application/json": components["schemas"]["ProblemDetails"];
+                        "text/json": components["schemas"]["ProblemDetails"];
+                    };
+                };
+            };
+        };
+        put?: never;
+        post?: never;
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
+    };
+    "/api/campaigns/{id}/create-posting": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        get?: never;
+        put?: never;
+        post: {
+            parameters: {
+                query?: never;
+                header?: never;
+                path: {
+                    id: number;
+                };
+                cookie?: never;
+            };
+            requestBody: {
+                content: {
+                    "application/json": components["schemas"]["JobPostingCreateDto"];
+                    "text/json": components["schemas"]["JobPostingCreateDto"];
+                    "application/*+json": components["schemas"]["JobPostingCreateDto"];
+                };
+            };
+            responses: {
+                /** @description Created */
+                201: {
+                    headers: {
+                        [name: string]: unknown;
+                    };
+                    content: {
+                        "text/plain": components["schemas"]["JobPostingGetDto"];
+                        "application/json": components["schemas"]["JobPostingGetDto"];
+                        "text/json": components["schemas"]["JobPostingGetDto"];
+                    };
+                };
+                /** @description Bad Request */
+                400: {
+                    headers: {
+                        [name: string]: unknown;
+                    };
+                    content: {
+                        "text/plain": components["schemas"]["ProblemDetails"];
+                        "application/json": components["schemas"]["ProblemDetails"];
+                        "text/json": components["schemas"]["ProblemDetails"];
+                    };
+                };
+                /** @description Not Found */
+                404: {
+                    headers: {
+                        [name: string]: unknown;
+                    };
+                    content: {
+                        "text/plain": components["schemas"]["ProblemDetails"];
+                        "application/json": components["schemas"]["ProblemDetails"];
+                        "text/json": components["schemas"]["ProblemDetails"];
+                    };
+                };
+            };
+        };
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
+    };
 }
 export type webhooks = Record<string, never>;
 export interface components {
-	schemas: {
-		CampaignPriority: number;
-		CandidateExperienceGetDto: {
-			companyName: string;
-			jobName: string;
-			/** Format: date-time */
-			dateStarted: string;
-			/** Format: date-time */
-			dateFinished?: string | null;
-			/** Format: int32 */
-			id: number;
-			/** Format: date-time */
-			createdAt: string;
-			/** Format: date-time */
-			updatedAt: string;
-		};
-		CandidateGetDto: {
-			firstName: string;
-			lastName: string;
-			experiences: components["schemas"]["CandidateExperienceGetDto"][];
-			/** Format: int32 */
-			id: number;
-			/** Format: date-time */
-			createdAt: string;
-			/** Format: date-time */
-			updatedAt: string;
-		};
-		ProblemDetails: {
-			type?: string | null;
-			title?: string | null;
-			/** Format: int32 */
-			status?: number | null;
-			detail?: string | null;
-			instance?: string | null;
-		};
-		RecruitmentCampaignCreateDto: {
-			name: string;
-			priority: components["schemas"]["CampaignPriority"];
-		};
-		RecruitmentCampaignGetDto: {
-			name: string;
-			priority: components["schemas"]["CampaignPriority"];
-			/** Format: int32 */
-			id: number;
-			/** Format: date-time */
-			createdAt: string;
-			/** Format: date-time */
-			updatedAt: string;
-		};
-	};
-	responses: never;
-	parameters: never;
-	requestBodies: never;
-	headers: never;
-	pathItems: never;
+    schemas: {
+        CampaignPostingGetDto: {
+            jobName: string;
+            /** Format: date-time */
+            datePosted: string;
+            description: string;
+            status: components["schemas"]["PostingStatus"];
+            /** Format: int32 */
+            id: number;
+            /** Format: date-time */
+            createdAt: string;
+            /** Format: date-time */
+            updatedAt: string;
+        };
+        CampaignPriority: number;
+        CandidateExperienceGetDto: {
+            companyName: string;
+            jobName: string;
+            /** Format: date-time */
+            dateStarted: string;
+            /** Format: date-time */
+            dateFinished?: string | null;
+            /** Format: int32 */
+            id: number;
+            /** Format: date-time */
+            createdAt: string;
+            /** Format: date-time */
+            updatedAt: string;
+        };
+        CandidateGetDto: {
+            firstName: string;
+            lastName: string;
+            experiences: components["schemas"]["CandidateExperienceGetDto"][];
+            /** Format: int32 */
+            id: number;
+            /** Format: date-time */
+            createdAt: string;
+            /** Format: date-time */
+            updatedAt: string;
+        };
+        JobPostingCreateDto: {
+            /** Format: date-time */
+            datePosted: string;
+            /** Format: int32 */
+            campaignId: number;
+            status: components["schemas"]["PostingStatus"];
+            jobName: string;
+            description: string;
+        };
+        JobPostingGetDto: {
+            jobName: string;
+            /** Format: date-time */
+            datePosted: string;
+            description: string;
+            status: components["schemas"]["PostingStatus"];
+            /** Format: int32 */
+            campaignId: number;
+            campaign: components["schemas"]["PostingCampaignDto"];
+            /** Format: int32 */
+            id: number;
+            /** Format: date-time */
+            createdAt: string;
+            /** Format: date-time */
+            updatedAt: string;
+        };
+        PostingCampaignDto: {
+            name: string;
+            priority: components["schemas"]["CampaignPriority"];
+            /** Format: int32 */
+            id: number;
+            /** Format: date-time */
+            createdAt: string;
+            /** Format: date-time */
+            updatedAt: string;
+        };
+        PostingStatus: number;
+        ProblemDetails: {
+            type?: string | null;
+            title?: string | null;
+            /** Format: int32 */
+            status?: number | null;
+            detail?: string | null;
+            instance?: string | null;
+        };
+        RecruitmentCampaignCreateDto: {
+            name: string;
+            priority: components["schemas"]["CampaignPriority"];
+        };
+        RecruitmentCampaignGetDto: {
+            name: string;
+            priority: components["schemas"]["CampaignPriority"];
+            postings: components["schemas"]["CampaignPostingGetDto"][];
+            /** Format: int32 */
+            id: number;
+            /** Format: date-time */
+            createdAt: string;
+            /** Format: date-time */
+            updatedAt: string;
+        };
+    };
+    responses: never;
+    parameters: never;
+    requestBodies: never;
+    headers: never;
+    pathItems: never;
 }
 export type $defs = Record<string, never>;
 export type operations = Record<string, never>;
