5d7cf2e7a1e7afcde6452bfa191de702d45370e5
Awraam <awraam.waskowski@gmail.com>
2025-06-19 11:29:43 +0200

    🚀 feat: Add unique constraint to stage meeting

---
+++ ezhire_api/ezhire_api/Migrations/20250619092835_MakeMeetingsUnique.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ezhire_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeMeetingsUnique : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_recruitment_stage_meeting_application_id",
+                table: "recruitment_stage_meeting");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_recruitment_stage_meeting_application_id_recruitment_stage_~",
+                table: "recruitment_stage_meeting",
+                columns: new[] { "application_id", "recruitment_stage_id" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_recruitment_stage_meeting_application_id_recruitment_stage_~",
+                table: "recruitment_stage_meeting");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_recruitment_stage_meeting_application_id",
+                table: "recruitment_stage_meeting",
+                column: "application_id");
+        }
+    }
+}
